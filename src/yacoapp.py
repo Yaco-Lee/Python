13f5ce2567fd01a201e7c4910991c2fffbfc27ca
Yaco <elsambadeixion@gmail.com>
2019-11-29 20:27:06 -0300

    Sumó lista de operaciones y se volvio extensible

--- src/yacoapp.py
+++ src/yacoapp.py
@@ -1,27 +1,52 @@
 # ACA ES DONDE VAMOS A DEFINIR LAS COSAS
 def main():
+  operaciones = {
+    "sumar": sumar,
+    "restar": restar,
+    "multiplicar": multiplicar,
+    "dividir": dividir
+  }
+  print("tenemos por ahora")
+  devuelveListaDeOperaciones(operaciones)
+
+
   print("que deseas?")
   accion = input()
+  print("poné el primer numero wacheen")
   primernumero = input()
+  print("ahora el segundo")
   segundonumero = input()
   x = int(primernumero)
   y = int(segundonumero)
-  result = 0
-
-  if accion == "sumar":
-   result = sumar(x,y)
-  elif accion == "restar":
-   result = restar(x,y)
-  else:
-   print("Por ahora solo sumo y resto wacho, awantia")
+  busquedaEnOperaciones = operaciones.get(accion)
+ 
+  result = busquedaEnOperaciones(x,y)
 
   print(result)
+
+
+
   
-def restar (x,y):
+def restar(x,y):
   return x-y
 
 def sumar(x, y):
   return x + y
+
+def multiplicar(x,y):
+  return x * y
+
+def dividir(x,y):
+  if y == 0:
+    print("no se puede dividir por cero... aún... ¬¬")
+    return 0
+  else:
+    return  x / y
+   
+
+def devuelveListaDeOperaciones(listaDeOperaciones):
+  for a in listaDeOperaciones:
+   print(a)
 
 
 ################################################
