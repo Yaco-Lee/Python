0a7f95d948d3e4f682d31dbaa0733ab45470aa5d
YacOnceApp <elsambadeixion@gmail.com>
2020-01-28 20:57:14 -0300

    defined usecases, added findstock
    Co-authored-by: bulfonjf <bulfonjf@gmail.com>

--- OnceApp/model/Gloria.py
+++ OnceApp/model/Gloria.py
@@ -3,38 +3,54 @@
 # This file contains two main sections, first one is the data of the actor, second one is the behavior
 
 class Gloria:
-    ### ---- DATA ---- ###
     
-    ### ---- BEHAVIOR ---- ###
     # input -> proceso de esa informacion -> output
     def __init__(self): 
+        ### ---- DATA ---- ###
         self.Stock = []
+        self.Revendedoras = []
     
+     ### ---- BEHAVIOR ---- ###
     def AddToStock(self, unaRopa):
         self.Stock.append(unaRopa)
     
     def RemoveFromStock(self, ciertaRopa):
         self.Stock.remove(ciertaRopa)
     
-    def MostrarStock(self):
+    def ShowStock(self):
         print(self.Stock)
-    
+        
+    def FindStock(self, consultaRopa):
+        if consultaRopa in self.Stock:
+            txt = "Si, tenemos {} en stock wachina"
+            print(txt.format(consultaRopa))
 
+
+        
+        
 
 # DEMO
 
 gloria = Gloria()
-print("El stock cuando creas Gloria")
-gloria.MostrarStock()
+print("El stock cuando creas la primer Gloria")
+gloria.ShowStock()
 
-print("Agrego una remera")
+print("Agrego una remera en la primer Gloria")
 gloria.AddToStock("una remera")
 print("Nuevo stock")
-gloria.MostrarStock()
+gloria.ShowStock()
 
-print("Quito una remera")
-print("Nuevo stock")
-gloria.RemoveFromStock("una remera")
-gloria.MostrarStock()
+print("qué estás buscando?")
+gloria.FindStock("una remera")
+
+
+
+
+# print("Quito una remera")
+# print("Nuevo stock")
+# gloria.RemoveFromStock("una remera")
+# gloria.ShowStock()
+
+
 
 
